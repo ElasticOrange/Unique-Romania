6e173bf51328d374e2998a8c27d14ef2cef72f6b
Vladimir Urucu <vladimir.urucu@gmail.com>
2014-11-05 19:05:02 +0200

    Add homepage unfinished

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,5 +1,39 @@
 @extends('layout.master')
 
 @section('content')
-    Hello <strong>Vladi</strong>!
+  
+
+<div class="row position-relative">
+	<img src="img/intro-pic.png"/>
+	<h1 class="title-main">Unique Romania</h1>
+</div>
+
+<div class="row background-grey">
+	<div class="col-xs-8">
+		<img class="bubble" src="img/buble-red-head.png"/>
+		<h2 class="title-second">Descopera Unique Romania</h2>
+		<p class="content-second">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+	</div>
+	<div class="col-xs-4">
+		<img src="img/logo.png"/>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-xs-8">
+		<img class="bubble" src="img/buble-red-head.png"/>
+		<h2 class="title-second">Castiga un premiu</h2>
+		<p class="content-second">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+	</div>
+	<div class="col-xs-4">
+		<img src="img/logo-camera.png"/>
+	</div>
+
+</div>
+<div class="row">
+	<div class="col-xs-12">
+		<button type="button" class="btn btn-default">Lanseaza aplicatia</button>
+	</div>
+</div>
+
 @stop

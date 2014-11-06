ee28e6671fc54a86748f2f639895af8f44173629
Vladimir Urucu <vladimir.urucu@gmail.com>
2014-11-06 18:12:46 +0200

    Add galery page without css

--- app/views/gallery.blade.php
+++ app/views/gallery.blade.php
@@ -1,5 +1,94 @@
 @extends('layout.master')
 
 @section('content')
-    Aici pui content
+    <div class="row header-image">
+    	<div class="col-xs-8 background-red">
+ 			<h1 class="">Unique Romania</h1>
+ 		</div>
+ 		<div class="col-xs-4 background-red">
+			<img src="/img/logo-white.png" />
+		</div>
+    </div>
+    
+    <div class="row background-grey">
+        <div class="col-xs-8">
+            <img class="bubble" src="img/buble-red-head.png"/>
+            <h2 class="title-second">Cum vad altii Romania</h2>
+            <p class="content-second">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu .</p>
+        </div>
+        
+       	<div class="col-xs-4 align-center">
+            <button type="button" class="btn btn-danger btn-md button-arata-ne text-white">Arata-ne cum vezi tu</button>
+        </div>
+    </div>
+    <div class="row background-grey">
+    	<hr>
+    </div>
+    <div class="row background-grey">
+    	<div class="input-group col-xs-10 center">
+      		<input class="form-control" type="email" placeholder="Cauta dupa nume">
+      		<div class="input-group-addon">
+      			<span class="glyphicon glyphicon-search"></span>
+      		</div>
+    	</div>
+    </div>
+    <div class="row background-grey">
+    	<div class="col-xs-2">
+    		<img class="" src="../img/profile-pic-default.png"/>
+    	</div>
+    		<h3 class="col-md-4 username-title">Corneliu Vasilescu</h3>
+    	<div class="col-md-4">
+	    	<span class="glyphicon glyphicon-pencil"></span>
+	    	<span class="glyphicon glyphicon-camera"></span>
+	    	<span class="glyphicon glyphicon-facetime-video"></span>
+	    	<span class="glyphicon glyphicon-chevron-down"></span>
+	    </div>
+	</div>
+    <div class="row background-grey">
+    	<hr>
+    </div>
+    <div class="row background-grey">
+    	<div class="col-xs-2">
+    		<img class="" src="../img/profile-pic-default.png"/>
+    	</div>
+    		<h3 class="col-md-4 username-title">Corneliu Vasilescu</h3>
+    	<div class="col-md-4">
+	    	<span class="glyphicon glyphicon-pencil"></span>
+	    	<span class="glyphicon glyphicon-camera"></span>
+	    	<span class="glyphicon glyphicon-facetime-video"></span>
+	    	<span class="glyphicon glyphicon-chevron-down"></span>
+	    </div>
+	</div>
+	<div class="row background-grey">
+    	<hr>
+    </div>
+    <div class="row background-grey">
+    	<div class="col-xs-2">
+    		<img class="" src="../img/profile-pic-default.png"/>
+    	</div>
+    		<h3 class="col-md-4 username-title">Corneliu Vasilescu</h3>
+    	<div class="col-md-4">
+	    	<span class="glyphicon glyphicon-pencil"></span>
+	    	<span class="glyphicon glyphicon-camera"></span>
+	    	<span class="glyphicon glyphicon-facetime-video"></span>
+	    	<span class="glyphicon glyphicon-chevron-down"></span>
+	    </div>
+	</div>
+	<div class="row background-grey">
+    	<hr>
+    </div>
+    <div class="row background-grey">
+        <div class="col-xs-12">
+            <img class="bubble" src="img/buble-red-head.png"/>
+            <h2 class="title-second">Nume articol</h2>
+            <p class="content-second">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu .</p>
+        </div>
+    </div>
+	    <div class="row background-grey">
+	    	<h2 class="title-second">Galerie Poze</h2>
+	    </div>
+
+
+
+
 @stop

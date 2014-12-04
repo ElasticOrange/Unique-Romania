6dbd148983fd14406b0db48023ed5096af6b38f3
Vladimir Urucu <vladimir.urucu@gmail.com>
2014-12-04 19:14:01 +0200

    Add endorser content text,video and pics

--- app/views/endorsergallery.blade.php
+++ app/views/endorsergallery.blade.php
@@ -26,15 +26,15 @@
 	    <div class="row background-grey row-padding">
 	        <div class="col-xs-2 ">
 	        	<div class="user-photo margin-full">
-	            	<img class="" src="/img/profile-pic-default.png"/>
+	            	<img class="" src="/img/vladpetreanu/2.jpg"/>
 	            </div>
 	        </div>
 
-            <h3 class="col-xs-4 eg-username-title">Ion Popescu</h3>
+            <h3 class="col-xs-4 eg-username-title">Vlad Petreanu</h3>
 
         </div>
         <div class="row background-grey">
-            <a href="https//www.zoso.ro/" class="link-black hand blog-link">www.example.com</a>
+            <a href="http://www.petreanu.ro/" class="link-black hand blog-link">http://www.petreanu.ro/</a>
         </div>
 
 
@@ -48,14 +48,13 @@
         </div>
         <div class="row background-grey">
             <div class="col-xs-11">
-                <p class="content-second margins">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-                 sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
-                 Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl
-                 ut aliquip ex ea commodo consequat.
-                 Duis autem vel eum iriure dolor in hendrerit in vulputate
-                 velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et
-                 accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore
-                 te feugait nulla facilisi.</p>
+                <p class="content-second margins">Privesc şi încerc să înţeleg cum vede lumea Flaviu Cernescu,
+                 slackliner român şi învingător de spaime. Are o poveste de spus şi o spune cu fiecare nouă
+                  provocare pe care o acceptă şi o supune – fie că traversează peste Transfăgărăşan, fie că
+                   dansează pe banda lui de 2,5 cm deasupra unui turn de răcire sau a unui preaplin de lac de
+                    acumulare. E plin de poveşti formidabile în jurul nostru. Suntem mereu în apropierea unor
+                     oameni speciali şi a unor locuri speciale. Trebuie doar să fim un pic atenţi, să ascultăm
+                      niţeluş, şi poveştile acestor oameni şi locuri ni se dezvăluie.</p>
             </div>
         </div>
 
@@ -72,34 +71,28 @@
                 <span class="eg-glyph glyphicon glyphicon-camera"></span>
             </div>
             <div class="col-xs-8">
-                <h4 class="eg-second-title">Vezi cum o vad si eu</h4>
+                <h4 class="eg-second-title">Galerie foto</h4>
             </div>
         </div>
         <div class="row background-grey">
             <div class="col-xs-12 hand margins">
-                <a href="http://images.carmelrealtycompany.com/webart/mls/thumb/748.jpg" class="fancybox">
-                    <img src="http://images.carmelrealtycompany.com/webart/mls/thumb/748.jpg" class="example-pic" />
+                <a href="/img/vladpetreanu/2.jpg." class="fancybox">
+                    <img src="/img/vladpetreanu/2.jpg." class="example-pic" />
                 </a>
-                <a href="http://images.carmelrealtycompany.com/webart/mls/thumb/748.jpg" class="fancybox">
-                    <img src="http://images.carmelrealtycompany.com/webart/mls/thumb/748.jpg" class="example-pic" />
+                <a href="/img/vladpetreanu/3.jpg." class="fancybox">
+                    <img src="/img/vladpetreanu/3.jpg." class="example-pic" />
                 </a>
-                <a href="http://images.carmelrealtycompany.com/webart/mls/thumb/748.jpg" class="fancybox">
-                    <img src="http://images.carmelrealtycompany.com/webart/mls/thumb/748.jpg" class="example-pic" />
+                <a href="/img/vladpetreanu/4.jpg." class="fancybox">
+                    <img src="/img/vladpetreanu/4.jpg." class="example-pic" />
                 </a>
-                <a href="http://images.carmelrealtycompany.com/webart/mls/thumb/748.jpg" class="fancybox">
-                    <img src="http://images.carmelrealtycompany.com/webart/mls/thumb/748.jpg" class="example-pic" />
+                <a href="/img/vladpetreanu/6.jpg." class="fancybox">
+                    <img src="/img/vladpetreanu/6.jpg." class="example-pic" />
                 </a>
-                <a href="http://images.carmelrealtycompany.com/webart/mls/thumb/748.jpg" class="fancybox">
-                    <img src="http://images.carmelrealtycompany.com/webart/mls/thumb/748.jpg" class="example-pic" />
+                <a href="/img/vladpetreanu/7.jpg." class="fancybox">
+                    <img src="/img/vladpetreanu/7.jpg." class="example-pic" />
                 </a>
-                <a href="http://images.carmelrealtycompany.com/webart/mls/thumb/748.jpg" class="fancybox">
-                    <img src="http://images.carmelrealtycompany.com/webart/mls/thumb/748.jpg" class="example-pic" />
-                </a>
-                <a href="http://images.carmelrealtycompany.com/webart/mls/thumb/748.jpg" class="fancybox">
-                    <img src="http://images.carmelrealtycompany.com/webart/mls/thumb/748.jpg" class="example-pic" />
-                </a>
-                <a href="http://images.carmelrealtycompany.com/webart/mls/thumb/748.jpg" class="fancybox">
-                    <img src="http://images.carmelrealtycompany.com/webart/mls/thumb/748.jpg" class="example-pic" />
+                <a href="/img/vladpetreanu/5.jpg." class="fancybox">
+                    <img src="/img/vladpetreanu/5.jpg." class="example-pic" />
                 </a>
             </div>
         </div>
@@ -115,10 +108,122 @@
                 <span class="eg-glyph glyphicon glyphicon-film"></span>
             </div>
             <div class="col-xs-8">
-                <h4 class="eg-second-title">Filmeaza ca si mine</h4>
+                <h4 class="eg-second-title">Video</h4>
+                <h4 class="eg-second-title video-title">Am întâlnit omul care învinge spaima</h4>
             </div>
             <div class="col-xs-6 eg-example-vid">
-                <iframe width="470" height="263" src="//www.youtube.com/embed/-dZGDy45ZeI" frameborder="0" allowfullscreen></iframe>
+                <iframe width="470" height="263" src="//www.youtube.com/embed/-_C7mmFFTeM" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
+        <!-- Line Break -->
+        <div class="row">
+            <div class="background-red">
+                <div class="line-break line-break-double"></div>
+            </div>
+        </div>
+        <!-- Line Break -->
+        <div class="row">
+            <div class="background-red">
+                <div class=" line-break"></div>
+            </div>
+        </div>
+        <!-- Line Break -->
+        <div class="row background-grey row-padding">
+            <div class="col-xs-2 ">
+                <div class="user-photo margin-full">
+                    <img class="" src="/img/vladpetreanu/2.jpg"/>
+                </div>
+            </div>
+
+            <h3 class="col-xs-4 eg-username-title">Mădălina şi Elis</h3>
+
+        </div>
+        <div class="row background-grey">
+            <a href="http://designist.ro/" class="link-black hand blog-link">http://designist.ro/</a>
+        </div>
+
+
+        <div class="row background-grey">
+            <div class="col-xs-2">
+                <span class="eg-glyph glyphicon glyphicon-pencil"></span>
+            </div>
+            <div class="col-xs-8">
+                <h4 class="eg-second-title">Despre Romania mea</h4>
+            </div>
+        </div>
+        <div class="row background-grey">
+            <div class="col-xs-11">
+                <p class="content-second margins">Experienţa acestei frumuseţi străvechi pe care o poţi trăi
+                 în prezent, a unei normalităţi şi armonii atemporale este cel mai mare dar (sau har) cu care 
+                 vine Hanul Balaban - si motivul pentru care o vizită aici e întâi de toate o (re)descoperire.
+                  Asa cum vă spuneam, ne plac lucrurile autentice si frumuseţea pe care o emană simplitatea, firescul.
+                   În plus, nu ai cum să întelegi tradiţionalul până nu încerci să-l trăieşti, să te raportezi familiar la el.
+                    Iar aici, aproape de crestele Bucegilor, o poţi face, într-o linişte şi un calm de început de lume,
+                    ritmate de talăngile turmelor.</p>
+            </div>
+        </div>
+
+       <!-- Line Break -->
+        <div class="row">
+            <div class="background-red">
+                <div class=" line-break"></div>
+            </div>
+        </div>
+        <!-- Line Break -->
+
+        <div class="row background-grey">
+            <div class="col-xs-2">
+                <span class="eg-glyph glyphicon glyphicon-camera"></span>
+            </div>
+            <div class="col-xs-8">
+                <h4 class="eg-second-title">Galerie foto</h4>
+            </div>
+        </div>
+        <div class="row background-grey">
+            <div class="col-xs-12 hand margins">
+                <a href="/img/designist/1.png." class="fancybox">
+                    <img src="/img/designist/1.png." class="example-pic" />
+                </a>
+                <a href="/img/designist/4.png." class="fancybox">
+                    <img src="/img/designist/4.png." class="example-pic" />
+                </a>
+                <a href="/img/designist/3.png." class="fancybox">
+                    <img src="/img/designist/3.png." class="example-pic" />
+                </a>
+                <a href="/img/designist/2.png." class="fancybox">
+                    <img src="/img/designist/2.png." class="example-pic" />
+                </a>
+                <a href="/img/designist/5.png." class="fancybox">
+                    <img src="/img/designist/5.png." class="example-pic" />
+                </a>
+                <a href="/img/designist/6.png." class="fancybox">
+                    <img src="/img/designist/6.png." class="example-pic" />
+                </a>
+                <a href="/img/designist/8.png." class="fancybox">
+                    <img src="/img/designist/8.png." class="example-pic" />
+                </a>
+                <a href="/img/designist/9.png." class="fancybox">
+                    <img src="/img/designist/9.png." class="example-pic" />
+                </a>
+            </div>
+        </div>
+        <!-- Line Break -->
+        <div class="row">
+            <div class="background-red">
+                <div class=" line-break"></div>
+            </div>
+        </div>
+        <!-- Line Break -->
+        <div class="row background-grey">
+            <div class="col-xs-2">
+                <span class="eg-glyph glyphicon glyphicon-film"></span>
+            </div>
+            <div class="col-xs-8">
+                <h4 class="eg-second-title">Video</h4>
+                <h4 class="eg-second-title video-title video-title2">Hanul Balaban</h4>
+            </div>
+            <div class="col-xs-6 eg-example-vid">
+                <iframe width="470" height="263" src="//www.youtube.com/embed/OfikPZ0sSJY?list=UUu7GfIyU1dCFTHrEGnfHsuQ" frameborder="0" allowfullscreen></iframe>
             </div>
         </div>
         <!-- Line Break -->
@@ -130,10 +235,114 @@
         <!-- Line Break -->
         <div class="row">
             <div class="background-red">
+                <div class="line-break line-break-double"></div>
+            </div>
+        </div>
+        <!-- Line Break -->
+        <div class="row background-grey row-padding">
+            <div class="col-xs-2 ">
+                <div class="user-photo margin-full">
+                    <img class="" src="/img/vladpetreanu/2.jpg"/>
+                </div>
+            </div>
+
+            <h3 class="col-xs-4 eg-username-title">Doru Panaitescu</h3>
+
+        </div>
+        <div class="row background-grey">
+            <a href="http://www.dorupanaitescu.ro/" class="link-black hand blog-link">http://www.dorupanaitescu.ro/</a>
+        </div>
+
+
+        <div class="row background-grey">
+            <div class="col-xs-2">
+                <span class="eg-glyph glyphicon glyphicon-pencil"></span>
+            </div>
+            <div class="col-xs-8">
+                <h4 class="eg-second-title">Despre Romania mea</h4>
+            </div>
+        </div>
+        <div class="row background-grey">
+            <div class="col-xs-11">
+                <p class="content-second margins"> Dragii mei, vine o vreme în viaţa unui om când începe să pună în balanţă 
+                  efortul şi recompensa pentru efortul depus. Am descoperit un loc în care priveliştea de la balcon 
+                   (dap, există şi un balcon în ecuaţie) îţi tăie răsuflarea şi te face să nu mai vrei să mai pleci de aici. 
+                    [de fapt este vorba de un cerdac, dar o să îmi fie iertată licenţa poetico-arhitectonică]. 
+                     Un loc în care cei care au recreat acest loc încărcat de tradiţie au fost atenţi la fiecare  
+                     detaliu pentru că vizitatorul (şi turistul cazat, după caz) să simtă că s-a întors nişte sute 
+                      de ani în urmă (în sensul bun, fireşte) .</p>
+            </div>
+        </div>
+
+       <!-- Line Break -->
+        <div class="row">
+            <div class="background-red">
                 <div class=" line-break"></div>
             </div>
         </div>
         <!-- Line Break -->
+
+        <div class="row background-grey">
+            <div class="col-xs-2">
+                <span class="eg-glyph glyphicon glyphicon-camera"></span>
+            </div>
+            <div class="col-xs-8">
+                <h4 class="eg-second-title">Galerie foto</h4>
+            </div>
+        </div>
+        <div class="row background-grey">
+            <div class="col-xs-12 hand margins">
+                <a href="/img/dorupanaitescu/2.jpg" class="fancybox">
+                    <img src="/img/dorupanaitescu/2.jpg" class="example-pic" />
+                </a>
+                <a href="/img/dorupanaitescu/3.jpg" class="fancybox">
+                    <img src="/img/dorupanaitescu/3.jpg" class="example-pic" />
+                </a>
+                <a href="/img/dorupanaitescu/1.jpg" class="fancybox">
+                    <img src="/img/dorupanaitescu/1.jpg" class="example-pic" />
+                </a>
+                <a href="/img/dorupanaitescu/5.jpg" class="fancybox">
+                    <img src="/img/dorupanaitescu/5.jpg" class="example-pic" />
+                </a>
+                <a href="/img/dorupanaitescu/4.jpg" class="fancybox">
+                    <img src="/img/dorupanaitescu/4.jpg" class="example-pic" />
+                </a>
+            </div>
+        </div>
+        <!-- Line Break -->
+        <div class="row">
+            <div class="background-red">
+                <div class=" line-break"></div>
+            </div>
+        </div>
+        <!-- Line Break -->
+        <div class="row background-grey">
+            <div class="col-xs-2">
+                <span class="eg-glyph glyphicon glyphicon-film"></span>
+            </div>
+            <div class="col-xs-8">
+                <h4 class="eg-second-title">Video</h4>
+                <h4 class="eg-second-title video-title video-title2">Un sat retras</h4>
+            </div>
+            <div class="col-xs-6 eg-example-vid">
+                <iframe width="470" height="263" src="//www.youtube.com/embed/djxNSWNW2gQ?list=UUu7GfIyU1dCFTHrEGnfHsuQ" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
+        <!-- Line Break -->
+        <div class="row">
+            <div class="background-red">
+                <div class=" line-break"></div>
+            </div>
+        </div>
+        <!-- Line Break -->
+        <div class="row">
+            <div class="background-red">
+                <div class="line-break line-break-double"></div>
+            </div>
+        </div>
+        <!-- Line Break -->
+
+
     @include('layout.footer')
 @stop
 
